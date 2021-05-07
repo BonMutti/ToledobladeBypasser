0797bf8f9c98c4b393417b729032cdd312c817e1
Bon <10662057+BonMutti@users.noreply.github.com>
2021-05-07 02:58:14 -0400

    Add files via upload

--- paywallDisabler.js
+++ paywallDisabler.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     setTimeout(function () {
-        $("body").removeClass("tp-modal-open");
         $(document).removeClass("tp-modal tp-backdrop tp-active lazy-transclude view view-offer view-show view-showed");
+        $("body").removeClass("tp-modal-open");
         $("div").removeClass("tp-modal tp-backdrop tp-active lazy-transclude view view-offer view-show view-showed")
     }, 3000);
 

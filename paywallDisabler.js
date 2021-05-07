b49f69f06a2bc9f469b7c124b6cb967787eaf7ce
Bon <10662057+BonMutti@users.noreply.github.com>
2021-05-07 02:45:27 -0400

    Added disabler file

---
+++ paywallDisabler.js
@@ -0,0 +1,28 @@
+// function removeTPModal() {
+//     $("body").removeClass("tp-modal-open");
+// }
+
+// removeTPModal();
+
+let TPParam = "";
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $("body").removeClass("tp-modal-open");
+        $(document).removeClass("tp-modal tp-backdrop tp-active lazy-transclude view view-offer view-show view-showed");
+        $("div").removeClass("tp-modal tp-backdrop tp-active lazy-transclude view view-offer view-show view-showed")
+    }, 2500);
+
+});
+
+setTimeout(function () {
+    TPParam = "";
+}, 6000);
+
+
+
+// www.toledoblade.com##.tp - modal
+// www.toledoblade.com##.tp - active.tp - backdrop
+
+// tp - modal - open
+// tp - modal tp - backdrop tp - active tp - active.tp - backdrop tp - active.tp - iframe - wrapper offer - 0 - 0ExG4 lazy - transclude
